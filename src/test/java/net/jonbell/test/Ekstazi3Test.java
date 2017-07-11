d8aad84381c7327a6e6ca3a0247f3dc6dd0420dc
Jonathan Bell <jbell@cs.columbia.edu>
2017-07-11 14:21:52 -0400

    simplify test

--- src/test/java/net/jonbell/test/Ekstazi3Test.java
+++ src/test/java/net/jonbell/test/Ekstazi3Test.java
@@ -7,6 +7,6 @@ import org.junit.Test;
 public class Ekstazi3Test {
 	@Test
 	public void testShouldDependOnClass3() throws Exception {
-		System.out.println("Value from c3: " + ((Class2)Class1.c2).c3.v);
+		System.out.println("Value from c3: " + Class1.c2.c3.v);
 	}
 }
